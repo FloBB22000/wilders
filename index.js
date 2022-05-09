39205f25f6ca108c5935d2083fcf7b55ae99331e
FloBB22000 <florence.goslino@sncf.fr>
2022-05-09 14:50:27 +0200

    wilderex

---
+++ index.js
@@ -0,0 +1,6 @@
+const names = ['Jonh','Paul','Joe','Polo'];
+import chalk from 'chalk';
+console.log(chalk.bgBlue('hello Paul !'));
+console.log(chalk.bgWhiteBright('hello John !'));
+console.log(chalk.bgYellow('hello Joe !'));
+console.log(chalk.bgYellow('hello Polo !'));
